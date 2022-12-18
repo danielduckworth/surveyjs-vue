4299f8bb82ae280bdcd99bd6652fe8752dd04518
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2022-12-19 03:03:24 +1100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div id="surveyElement">
+            <SurveyComponent />
+        </div>
+    </div>
+</template>
+<script>
+    import SurveyComponent from "./components/SurveyComponent";
+    export default {
+        name: "App",
+        components: {
+            SurveyComponent
+        }
+    };
+</script>
---
+++ src/components/SurveyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <survey :survey="survey"  />
+</template>
+<script>
+    import Vue from "vue";
+    import { StylesManager, Model } from "survey-vue";
+    import "survey-vue/defaultV2.css";
+    import "./index.css";
+    import { json } from "./json";
+    StylesManager.applyTheme("defaultV2");
+    export default {
+        name: "surveyjs-component",
+        data() {
+            const survey = new Model(json);
+            return {
+                survey: survey
+            };
+        }
+    };
+</script>
+<style scoped>
+</style>
